048a6983b30a3dea0e10a2a531004d2f96b4a9dd
Jessyca Toselli <jfamedeiros@gmail.com>
2021-05-29 14:35:24 -0300

    Initial commit

---
+++ app/src/main/java/com/jess/santanderdevweek/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.jess.santanderdevweek
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Item 1 clicked")
+                true
+            }
+            R.id.item_2 -> {
+                Log.d("CLICK", "Item 2 clicked")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
